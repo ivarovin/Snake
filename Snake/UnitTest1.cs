838caa521b5ddf69bc5563ad2610c17ffb57cd95
Iván Moreno <imoreno.owo@gmail.com>
2023-11-11 11:56:25 +0100

    build: create project

---
+++ Snake/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace Snake;
+
+// 1.Head moves forward
+// 2.Head can turn left or right or up or down
+// 3.Head can't turn back
+// 4.Fruit appear on the field
+// 5.Snake can eat fruit
+// 7.Snake can't go through walls (dies)
+// 8.Snake can't go through itself (dies)
+// 9.Snake grows when eats fruit
+// 10.Snake body moves with head
+
+public class Tests
+{
+    [Test]
+    public void Test1()
+    {
+        
+    }
+}
